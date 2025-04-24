bbb2e48399b3b3e4f337a8647c91c93fca26dafd
Yolls24 <jruelle@hotmail.fr>
2025-04-24 15:24:21 +0200

    Add files via upload

---
+++ exo2.rb
@@ -0,0 +1,25 @@
+Last login: Thu Apr 24 15:07:29 on ttys000 
+➜  ~ irb 
+irb(main):001* Bonjour, monde ! 
+irb(main):002* et avec une voix sexy, ca donne : Bonjour, monde !
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
---
+++ exo_01.rb
@@ -0,0 +1,26 @@
+Last login: Thu Apr 24 15:07:03 on ttys000 
+➜  ~ irb 
+irb(main):001> Bonjour, monde !
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
